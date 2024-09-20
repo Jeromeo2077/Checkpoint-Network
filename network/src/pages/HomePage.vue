40cf2538bdd1ebb14aa471a24118b7c042619875
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-20 10:55:11 -0600

    Emptied out the template section for the HomePage

--- network/src/pages/HomePage.vue
+++ network/src/pages/HomePage.vue
@@ -3,14 +3,10 @@
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container">
+    <section class="row">
+
+    </section>
   </div>
 </template>
 
