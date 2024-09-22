e8f62347cc034a6992b064483d2b9a4236679d67
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-21 19:19:22 -0600

    Removed template footer

--- network/src/App.vue
+++ network/src/App.vue
@@ -11,13 +11,11 @@ import { AppState } from './AppState.js';
   <main>
     <router-view />
   </main>
-  <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
-  </footer>
 </template>
 
 <style lang="scss">
 @import '@/assets/scss/main.scss';
+
 :root {
   --main-height: calc(100vh - 32px - 64px);
 }
