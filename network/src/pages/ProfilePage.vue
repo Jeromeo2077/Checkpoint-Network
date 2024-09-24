fa3b78b6056fc1303b3e75a72549dbd70aea7d84
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-23 21:36:23 -0600

    Formatted the Profile Update Form

--- network/src/pages/ProfilePage.vue
+++ network/src/pages/ProfilePage.vue
@@ -109,48 +109,48 @@ async function getAds() {
         <a class="m-3" :href="profile.linkedin">LinkedIn</a>
       </div>
       <div>
-
+        <div class="m-3"></div>
       </div>
       <h3 class="mt-3 mb-3">Profile Update Form</h3>
       <form v-if="account.id == profile?.id" @submit.prevent="updateAccount()">
-        <div>
+        <div class="m-3">
           <input v-model="editableAccountData.email" type="text" name="accountEmail" id="accountEmail"
             class="form-control-sm mx-3" maxlength="500" placeholder="Email...">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.name" type="text" name="accountName" id="accountName"
             class="form-control-sm mx-3" maxlength="100" placeholder="Name...">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.picture" type="url" name="accountPicture" id="accountPicture"
             class="form-control-sm mx-3" maxlength="500" placeholder="Profile Picture URL...">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <textarea v-model="editableAccountData.bio" name="accountBio" id="accountBio" class="form-control-sm mx-3"
             maxlength="1000" placeholder="Biography..."></textarea>
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.coverImg" type="url" name="accountCoverImg" id="accountCoverImg"
             class="form-control-sm mx-3" maxlength="500" placeholder="Cover Image URL..."
             default="https://images.unsplash.com/photo-1586829135343-132950070391?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.github" type="url" name="accountGithub" id="accountGithub"
             class="form-control-sm mx-3" maxlength="500" placeholder="Github URL...">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.linkedin" type="url" name="accountLinkedin" id="accountLinkedin"
             class="form-control-sm mx-3" maxlength="500" placeholder="LinkedIn URL...">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.resume" type="url" name="accountResume" id="accountResume"
             class="form-control-sm mx-3" maxlength="500" placeholder="Resume URL...">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <input v-model="editableAccountData.class" type="text" name="accountClass" id="accountClass"
             class="form-control-sm mx-3" maxlength="100" placeholder="Graduating Class">
         </div>
-        <div class="mb-3">
+        <div class="m-3">
           <label for="accountGraduated" class="form-label me-3">Have You Graduated?</label>
           <input v-model="editableAccountData.graduated" type="checkbox" name="accountGraduated" id="accountGraduated">
         </div>
